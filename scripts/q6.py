d0d1ea6c5ba1e6759f00ce3143de60751280168e
Layza Carneiro <layza.mrcarneiro@gmail.com>
2026-04-15 23:42:39 -0300

    finalizada questao 6

--- scripts/q6.py
+++ scripts/q6.py
@@ -31,13 +31,16 @@ def to_gray_manual(img):
 
 def quantize_image(img, niveis):
     altura, largura = img.shape
+
+    # Imagem de saída
     result = np.zeros((altura, largura), dtype=np.uint8)
 
     print(f"Quantizando para {niveis} níveis...")
 
+    # Define o tamanho de cada intervalo de intensidade
     passo = int(256 / niveis)
 
-    # fator de expansão para ocupar [0,255]
+    # Fator de expansão para redistribuir os níveis no intervalo [0,255]
     escala = 255 / (256 - passo)
 
     for y in range(altura):
@@ -45,12 +48,13 @@ def quantize_image(img, niveis):
 
             valor = img[y, x]
 
-            # quantização padrão
+            # Quantização: reduz os níveis de intensidade
             q = int(valor / passo) * passo
 
-            # expansão de contraste (ESSENCIAL)
+            # Expansão de contraste: "empurra" os valores para preto/branco
             novo_valor = int(q * escala)
 
+            # Clipping de segurança
             if novo_valor > 255:
                 novo_valor = 255
 
@@ -59,35 +63,61 @@ def quantize_image(img, niveis):
     return result
 
 # -------------------------
+# 3. Redimensionamento proporcional
+# -------------------------
+
+def resize_proporcional(img, tamanho_max=512):
+    altura, largura, _ = img.shape
+
+    # Mantém proporção original
+    if altura > largura:
+        nova_altura = tamanho_max
+        nova_largura = int(largura * (tamanho_max / altura))
+    else:
+        nova_largura = tamanho_max
+        nova_altura = int(altura * (tamanho_max / largura))
+
+    return cv2.resize(img, (nova_largura, nova_altura))
+
+
+
+# -------------------------
 # Carregar imagem
 # -------------------------
 
-img = cv2.imread('fotos/macaco.png')
+img = cv2.imread('fotos/praia.png')
 
 print("Imagem carregada!")
+
+# Redimensionamento para melhorar desempenho
+img = resize_proporcional(img)
+print("Imagem redimensionada!")
+
 
 # -------------------------
 # Pipeline
 # -------------------------
 
+# Conversão para cinza
 img_gray = to_gray_manual(img)
 
-# níveis de quantização
+# Lista de níveis de quantização
 niveis_lista = [64, 32, 16, 8, 4, 2]
 
 imagens = []
 titulos = []
 
-# original
+# Imagem original (8 bits = 256 níveis)
 imagens.append(img_gray)
 titulos.append("(a) 256 níveis (8-bit)")
 
-# quantizações
+# Aplica quantização para cada nível
 for n in niveis_lista:
     img_q = quantize_image(img_gray, n)
 
     imagens.append(img_q)
     titulos.append(f"{n} níveis")
+
 
 # -------------------------
 # Exibição
@@ -106,7 +136,7 @@ plt.show()
 
 
 # -------------------------
-# Mostrar resultado
+# Salvar resultado
 # -------------------------
 
 plt.figure(figsize=(18,5))
